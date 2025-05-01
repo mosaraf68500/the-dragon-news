4d67aa03ad2d741c837395c6141cb2824360bce5
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-05-01 12:12:48 +0600

    date

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,16 @@
 import React from 'react'; 
 
 import logo from "../../assets/logo.png"
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
-        <div className='flex flex-col justify-center items-center py-4'>
+        <div className='flex flex-col justify-center items-center py-4 space-y-2'>
             <div>
                 <img src={logo} alt="" />
             </div>
-            <h1>Journalism Without Fear or Favour</h1>
+            <h1 className='text-accent'>Journalism Without Fear or Favour</h1>
+            <p className='text-accent font-bold'>{format(new Date(),"EEEE , MMMM dd , yyyy")}</p>
 
         </div>
     );
